8c81a4acc6a238fed51ee813fcc9ef418a9a6021
Michael Osarodion <macmive@gmail.com>
2020-09-08 19:10:12 +0100

    inital commit

---
+++ todo.js
@@ -0,0 +1,46 @@
+// UI variables
+const form = document.getElementById('task-form');
+const taskInput = document.getElementById('task');
+const filter = document.getElementById('filter');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+
+
+
+// call to load all event listeners
+loadEventListeners();
+
+// loadEventListeners funtion
+function loadEventListeners() {
+    // Add task event
+    form.addEventListener('submit', addTask);
+}
+
+// addTask function
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('No task added!');
+    } else {
+        // create li element
+        const li = document.createElement('li');
+        // add a class
+        li.className = 'collection-item';
+        // create text node and append to li
+        li.appendChild(document.createTextNode(taskInput.value));
+        // create link element
+        const link = document.createElement('a');
+        // add class, secondary-content: shifts elements to the right in materialise
+        link.className = 'delete-item secondary-content';
+        // add icon html
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // append link to li
+        li.appendChild(link);
+
+        // append li to ul
+        taskList.appendChild(li);
+        // clear input
+        taskInput.value = '';
+    }
+
+    e.preventDefault();
+}
